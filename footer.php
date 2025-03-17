decc4c03e6da612c1c8650162db494ad2009c1da
Bhanu <amantomarr@gmail.com>
2025-03-18 02:21:09 +0530

    fixed

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<footer>
-    <p>&copy; 2025 PharmEasy Clone. All rights reserved.</p>
+<footer> <Br> <Br>
+  <center>   <p>&copy; 2025 PharmEasy Clone. All rights reserved.</p> </center>
 </footer>
 </body>
 </html>
